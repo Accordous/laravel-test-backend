f79699ae4ce134a953c13ee8fb67f565964be61a
Cristian Anderson Oliveira Bernardes <cristian.aob@gmail.com>
2020-11-03 21:27:29 -0300

    Criado Logs

--- test-backend/app/Repositories/ContractRepository.php
+++ test-backend/app/Repositories/ContractRepository.php
@@ -9,4 +9,14 @@ class ContractRepository extends AbstractRepository
 {
     protected $model = Contract::class;
     protected $myRequest = ContractRequest::class;
+
+    public function all()
+    {
+        $data =  Contract::with('property')->get();
+        if(sizeof($data) > 0){
+            return response()->json($data);
+        }else {
+            return response()->json(['error' => trans('messages.404')], 404);
+        }
+    }
 }
